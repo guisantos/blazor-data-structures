f435dd442883cef3a0c09c3309024d3ef4764c4b
Guilherme Santos <guilherme.santos@planetpayment.com>
2020-04-21 16:32:39 +0100

    Added Stack screen and funcionalities

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,6 +24,7 @@ namespace VisualDataStructure
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<LinkedListService>();
+            services.AddSingleton<StackService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
